28631fef8d6a53dcec3be6755a6449661d608a1b
gilbertghang <gghang@gmail.com>
2018-05-13 23:43:51 -0400

    Build mta gtfs real-time url from MTA_KEY system environment variable

--- src/main/java/com/gilbertghang/app/App.java
+++ src/main/java/com/gilbertghang/app/App.java
@@ -1,13 +1,33 @@
 package com.gilbertghang.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import com.google.transit.realtime.NyctSubway;
+import com.google.transit.realtime.GtfsRealtime;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class App
 {
-    public static void main( String[] args )
+
+    private static String BASE_URL = "http://datamine.mta.info/mta_esi.php?key=%s&feed_id=%d";
+
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        URL mtaGtfsUrl = null;
+        try {
+            mtaGtfsUrl = buildMtaGtfsUrl(26);
+        } catch (MalformedURLException me) {
+            System.out.println("Could not build url");
+            me.printStackTrace();
+            return;
+        }
+
+        System.out.println(mtaGtfsUrl);
+    }
+
+    private static URL buildMtaGtfsUrl(int feedId) throws MalformedURLException {
+        String mtaApiKey = System.getenv("MTA_KEY");
+        String mtaGtfsDataUrlString = String.format(BASE_URL, mtaApiKey, feedId);
+        return new URL(mtaGtfsDataUrlString);
     }
 }
